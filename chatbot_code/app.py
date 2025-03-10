676b41e0f8d460aa480bbffff79663951a10920e
dev123 <hyunho3445@gmail.com>
2025-03-10 19:31:27 +0900

    ver1-1. code chore

--- chatbot_code/app.py
+++ chatbot_code/app.py
@@ -8,12 +8,10 @@ from company_dict import company_dict
 from deep_translator import GoogleTranslator
 
 inferenceProfileArn= os.getenv("BEDROCK_INFERENCE_PROFILE_ARN")
-
 app = Flask(__name__)
-
 bedrock_client = boto3.client("bedrock-runtime", region_name="ap-northeast-2")
 
-#### 주식 가격 관련 함수 ####
+### 주식 가격 관련 함수 ###
 def get_stock_symbol(company_name):
     result = search(company_name)
     quotes = result.get("quotes", [])
@@ -31,43 +29,8 @@ def get_currency(symbol):
         return "원"
     return "달러"
 
-## dic 사용용
-# def find_company_symbol(name):
-#     name_list = name.lower().split()
-#     for nl in name_list:
-#         if nl in company_dict:
-#             return company_dict.get(nl)
-#         else:
-#             return None
-
 def translate_to_english(translate):
     return GoogleTranslator(source='ko', target='en').translate(translate)
-
-# def find_company_symbol(name):
-#     if not name.isascii():
-#         name = translate_to_english(name)
-#     result = search(name)
-#     if result and 'quotes' in result and result["quotes"]:
-#         return result['quotes'][0]['symbol']
-#     return None
-
-# def find_company_symbol(name):
-#     """
-#     User input value translate
-#     """
-#     name_list = name.split()
-#     for ln in name_list:    
-#         if not name.isascii(): 
-#             symbol_correct = []
-#             name = translate_to_english(name)
-#             symbol_correct.append(name)
-#     symbol_correct2 = "".join(symbol_correct).split()
-#     results = []
-#     for ln2 in symbol_correct2:
-#         result = search(ln2)
-#         if result and 'quotes' in result and result["quotes"]:
-#             results.append(result['quotes'][0]['symbol'])
-#     return results if results else None
 
 def find_company_symbol(name):
     name_list = name.split()
@@ -82,8 +45,9 @@ def find_company_symbol(name):
     if result and 'quotes' in result and result["quotes"]:
         return result['quotes'][0]['symbol']
     return None
-#### ------------------ ####
+### ------------------ ###
 
+### 주식 가격 출력 response ###
 def chatbot_response(user_input):
     company_name = find_company_symbol(user_input)
 
@@ -129,7 +93,9 @@ def chatbot_response(user_input):
     ai_response = json.loads(response["body"].read())["content"][0]["text"]
 
     return stock_info if stock_info else ai_response.strip()
+### ------------------ ###
 
+### 일반 평문 대답 ###
 def chatbot_response2(user_input2):
     prompt = (
         f"너는 AI 비서야. 질문에 대해 친절하고 유익한 답변을 해줘."
@@ -161,6 +127,7 @@ def chatbot_response2(user_input2):
     ai_response = json.loads(response["body"].read())["content"][0]["text"]
 
     return ai_response.strip()
+### --------------- ###
 
 #### Flask 엔드포인트 ####
 @app.route("/chat", methods=["POST"])
@@ -175,14 +142,17 @@ def chat():
     if not user_input:
         return jsonify({"error": "빈 메시지입니다."}), 400
 
-    stock_price_keywords = ["주가", "가격"]
+    """
+    주식가격 질문 시 -> chatbot_response
+    일반 평문 질문 시 -> chatbot_response2
+    """
+    stock_price_keywords = ["주가", "가격", "주식가격", "주식 가격"]
     if any(keyword in user_input for keyword in stock_price_keywords):
         response = chatbot_response(user_input)
     else:
         response = chatbot_response2(user_input)
 
     return jsonify({"response": response})
-
 #### ---------------- ####
 
 @app.route("/")
