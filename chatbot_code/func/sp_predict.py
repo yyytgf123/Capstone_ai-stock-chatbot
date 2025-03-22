29fb1143cc490f358cc08ea6c358daf40b5149b1
dev123 <hyunho3445@gmail.com>
2025-03-22 23:02:34 +0900

    ver5-4~6. sp_predict - dataset setting2

--- chatbot_code/func/sp_predict.py
+++ chatbot_code/func/sp_predict.py
@@ -6,6 +6,15 @@ from yahooquery import search
 from deep_translator import GoogleTranslator
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
+import io
+import json
+
+###  Dataframe -> test/csv ###
+def dataframe_to_bytes(df):
+    buffer = io.StringIO()
+    df.to_csv(buffer, index=False, header=False)
+    return buffer.getvalue()
+### ------------------- ###
 
 ### find symbol ###
 def get_stock_symbol(company_name):
@@ -33,6 +42,18 @@ def find_company_symbol(name):
     return None
 ### ---------- ###
 
+### close value return ###
+def stock_close(symbol):
+    today = datetime.today()
+    formatted_today = today.strftime("%Y-%m-%d")
+    two_months_ago = today - relativedelta(months=6)
+    formatted_three_months_ago = two_months_ago.strftime("%Y-%m-%d")
+
+    df = yf.download(symbol, start=formatted_three_months_ago, end=formatted_today)
+    
+    return df
+### ----------------- ###
+
 ### get .csv ###
 def stock_data(symbol):
     today = datetime.today()
@@ -40,10 +61,10 @@ def stock_data(symbol):
     two_months_ago = today - relativedelta(months=6)
     formatted_three_months_ago = two_months_ago.strftime("%Y-%m-%d")
 
-    df = yf.download(symbol, start=formatted_three_months_ago, end=formatted_today)    
     """
-    MACD DataSet
+    Predict DataSet
     """
+    df = yf.download(symbol, start=formatted_three_months_ago, end=formatted_today)   
     df["MA9"] = df['Close'].rolling(window=9).mean()
     df["MA12"] = df['Close'].rolling(window=12).mean()
     df["MA26"] = df['Close'].rolling(window=26).mean()
@@ -57,10 +78,54 @@ def stock_data(symbol):
     return df
 ### ---------- ###
 
+### compare data ###
+def compare_data(symbol):
+    today = datetime.today()
+
+    #현재 - 내일 주가예측, 추후 - 기간 user_input으로 받아 주가예측
+    n_today = today + relativedelta(days=1)    
+    formatted_n_today = n_today.strftime("%Y-%m-%d")
+
+    df = pd.DataFrame()
+
+    #주말 계산 필요
+    #ma9
+    ma9_date = today - relativedelta(days=11)
+    formatted_ma9_date = ma9_date.strftime("%Y-%m-%d")
+    df_ma9 = yf.download(symbol, start=formatted_ma9_date, end=formatted_n_today)
+    df["MA9"] = df_ma9["Close"].rolling(window=9).mean()
+
+    #ma12
+    ma12_date = today - relativedelta(days=16)
+    formatted_ma12_date = ma12_date.strftime("%Y-%m-%d")
+    df_ma12 = yf.download(symbol, start=formatted_ma12_date, end=formatted_n_today)
+    df["MA12"] = df_ma12["Close"].rolling(window=12).mean()
+
+    #ma21
+    ma21_date = today - relativedelta(days=29)
+    formatted_mad21_date = ma21_date.strftime("%Y-%m-%d")
+    df_ma21 = yf.download(symbol, start=formatted_mad21_date, end=formatted_n_today)
+    df["MA21"] = df_ma21["Close"].rolling(window=21).mean()
+
+    #vol_ma5
+    vol_ma5 = today - relativedelta(days=7)
+    formatted_vol_ma5 = vol_ma5.strftime("%Y-%m-%d")
+    df_vol_ma5 = yf.download(symbol, start=formatted_vol_ma5, end=formatted_n_today)
+    df["Vol_MA5"] = df_vol_ma5["Volume"].rolling(window=5).mean()
+
+    #pct_change
+    pct_change = today - relativedelta(days=3)
+    formatted_pct_change = pct_change.strftime("%Y-%m-%d")
+    pct_change = yf.download(symbol, start=formatted_pct_change, end=formatted_n_today)
+    df["Pct_change"] = pct_change["Close"].pct_change()
+    
+    df.dropna(inplace=True)
+
+    return df
+
 ### data_set setting -> training ###
 import sagemaker
 from sklearn.model_selection import train_test_split
-from sagemaker.session import s3_input
 
 def sagemaker_training(user_input):
     symbol = find_company_symbol(user_input)
@@ -68,8 +133,11 @@ def sagemaker_training(user_input):
     data_set sagemaker s3 upload
     """
     df = stock_data(symbol)
-    df.to_csv("stock_data.csv", index=False, header=False, ) #df -> .csv 변경 -> data에 저장
+    df.to_csv("stock_data.csv", index=False, header=False) #df -> .csv 변경 -> data에 저장
     data_to_read = pd.read_csv("stock_data.csv", delimiter=";") #csv -> pd dataFrame
+
+    dy = stock_close(symbol)
+    y = dy['Close'].shift(-1) #다음 날 종가를 예측하기 위해 -1씩 밀기
 
     train_data, test_data = train_test_split(data_to_read, test_size=0.2) #8:2로 나눔
 
@@ -81,6 +149,9 @@ def sagemaker_training(user_input):
     # role = "arn:aws:iam::047719624346:role/chatbot-sagemaker-policy"    
     
     train_file_path = 'train.csv'
+    #df == X
+    #input 값 - MA9, MA12, MA26, Vol_MA5, Pct_Change, 타겟 값 - Close(예측할 값)
+    train_data = pd.concat([df, y], axis=1)
     train_data.to_csv(train_file_path, index=False, header=False) #index, header 불필요 데이터 제거
     s3_train_data = sagemaker_session.upload_data(path=train_file_path, bucket=bucket, key_prefix=prefix+'/train')
 
@@ -95,11 +166,12 @@ def sagemaker_training(user_input):
     container = sagemaker.image_uris.retrieve("xgboost", sagemaker_session.boto_region_name, "latest") #xgboost:latest 사용
 
     traindata_bucket = "s3://chatbot-sagemaker-s3/output-data"
+
     #docker에서 제공하는 sagemaker.estimator.Estimator 사용, "Tensorflow" 고려
     xgboost = sagemaker.estimator.Estimator(container,      
                                             role = "arn:aws:iam::047719624346:role/chatbot-sagemaker-policy",
                                             train_instance_count = 2,
-                                            train_instance_type = "ml.c4.xlarge",
+                                            train_instance_type = "ml.m5.xlarge",
                                             output_path = traindata_bucket,
                                             sagemaker_session=sagemaker_session) #sagemaker 모델 학습, 데이터 저장, 배포 관리 -> 세션 전달
 
@@ -109,11 +181,35 @@ def sagemaker_training(user_input):
                                 min_child_width=6, #리프 노드가 분할되기 위한 필요한 최소 샘플 가중치 합
                                 subsample=0.6, #각 트리를 학습할 때 사용하는 샘플링 비율
                                 silent=0, #학습 중 로그 출력 
-                                objective='reg:squarederror', #최적화할 목표 함수(손실 함수), reg:linear -> MSE 기반 회귀
+                                objective='reg:linear', #최적화할 목표 함수(손실 함수), reg:linear -> MSE 기반 회귀
                                 num_round=100) #트리의 개수
 
     s3_input_train_data = sagemaker.TrainingInput(s3_data=s3_train_data, content_type="csv")
     xgboost.fit({'train':s3_input_train_data})
+
+    """
+    모델 배포
+    """
+    from sagemaker.serializers import CSVSerializer
+
+    #배포
+    xgboost_deploy = xgboost.deploy(initial_instance_count=1,
+                                    instance_type='ml.m5.xlarge')
+    endpoint_name = xgboost_deploy.endpoint_name #response에 넣을 값
+    print(endpoint_name)
+
+    #비교 데이터 input
+    runtime = boto3.client('runtime.sagemaker')
+    
+    test_data = compare_data(symbol)    
+    type_trans_test_data = dataframe_to_bytes(test_data)
+     
+    response = runtime.invoke_endpoint(EndpointName=endpoint_name,
+                                       ContentType='text/csv',
+                                       Body=type_trans_test_data)
+    result = json.loads(response['Body'].read().decode())
+    print(result)
+    
 ### ------------------------ ###
 
 sagemaker_training("애플")
--- chatbot_code/func/test.py
+++ chatbot_code/func/test.py
@@ -1,3 +1,62 @@
-from sagemaker import get_execution_role
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+import yfinance as yf
+import pandas as pd
+import pickle
 
-print(get_execution_role())
+def compare_data(symbol):
+    today = datetime.today()
+
+    #현재 - 내일 주가예측, 추후 - 기간 user_input으로 받아 주가예측
+    n_today = today + relativedelta(days=1)    
+    formatted_n_today = n_today.strftime("%Y-%m-%d")
+
+    df = pd.DataFrame()
+
+    #주말 계산 필요
+    #ma9
+    ma9_date = today - relativedelta(days=11)
+    formatted_ma9_date = ma9_date.strftime("%Y-%m-%d")
+    df_ma9 = yf.download(symbol, start=formatted_ma9_date, end=formatted_n_today)
+    df["MA9"] = df_ma9["Close"].rolling(window=9).mean()
+
+    #ma12
+    ma12_date = today - relativedelta(days=16)
+    formatted_ma12_date = ma12_date.strftime("%Y-%m-%d")
+    df_ma12 = yf.download(symbol, start=formatted_ma12_date, end=formatted_n_today)
+    df["MA12"] = df_ma12["Close"].rolling(window=12).mean()
+
+    #ma21
+    ma21_date = today - relativedelta(days=29)
+    formatted_mad21_date = ma21_date.strftime("%Y-%m-%d")
+    df_ma21 = yf.download(symbol, start=formatted_mad21_date, end=formatted_n_today)
+    df["MA21"] = df_ma21["Close"].rolling(window=21).mean()
+
+    #vol_ma5
+    vol_ma5 = today - relativedelta(days=7)
+    formatted_vol_ma5 = vol_ma5.strftime("%Y-%m-%d")
+    df_vol_ma5 = yf.download(symbol, start=formatted_vol_ma5, end=formatted_n_today)
+    df["Vol_MA5"] = df_vol_ma5["Volume"].rolling(window=5).mean()
+
+    #pct_change
+    pct_change = today - relativedelta(days=3)
+    formatted_pct_change = pct_change.strftime("%Y-%m-%d")
+    pct_change = yf.download(symbol, start=formatted_pct_change, end=formatted_n_today)
+    df["Pct_change"] = pct_change["Close"].pct_change()
+    
+    df.dropna(inplace=True)
+
+    return df
+
+import io
+def dataframe_to_bytes(df):
+    buffer = io.StringIO()
+    df.to_csv(buffer, index=False, header=False)
+    return buffer.getvalue()
+
+stock = compare_data("AAPL")
+
+
+trans_stock = dataframe_to_bytes(stock)
+
+print(trans_stock)
